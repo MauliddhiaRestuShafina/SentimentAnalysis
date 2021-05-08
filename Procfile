b7424abb333a83dc5966e5ad73d3c25b3cea0f64
MauliddhiaRestuShafina <mrshafina@gmail.com>
2021-05-08 23:47:13 +0700

    masih mencobaaa

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn run:application
+gunicorn app:application --preload -b 0.0.0.0:5000 
--- application.py
+++ application.py
@@ -61,5 +61,8 @@ def search():
 
 
 # run the app.
-if __name__ == '__main__':
-    application.run()
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    #application.debug = True
+    application.run(host='0.0.0.0')
